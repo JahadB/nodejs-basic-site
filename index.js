0d840d9a04611ddab1b17096b4bac8ea09f55d48
JahadB <beylercahad@gmail.com>
2024-07-30 16:13:04 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
+const PORT = process.env.PORT;
+
+//Getting current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, "public", "index.html");
+            }else if (req.url === '/about') {
+                filePath = path.join(__dirname, "public", "about.html");
+            }
+            else if (req.url === '/contact-me') {
+                filePath = path.join(__dirname, 'public', 'contact-me.html');
+            }
+            else {
+                filePath = path.join(__dirname, 'public', '404.html');
+                res.statusCode = 404;
+            }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('Method not allowed');
+        }
+
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'})
+        res.end("<h1>Server Error</h1>");
+    } 
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}
+);
